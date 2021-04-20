345b788157c29eea75c5ebd082775c9cde9392b4
peviroy <peviroy@outlook.com>
2021-04-20 19:44:54 +0800

    add: append .local/bin to PATH

--- .config/zsh/env.sh
+++ .config/zsh/env.sh
@@ -8,3 +8,5 @@ PATH="$NPM_PACKAGES/bin:$PATH"
 # Unset manpath so we can inherit from /etc/manpath via the `manpath`
 unset MANPATH # delete if you already modified MANPATH elsewhere in your config
 MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+PATH=$PATH:$HOME/.local/bin
